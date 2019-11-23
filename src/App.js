c9f93a1a0c67ac9cfd7621b719782d5d4c5bbadc
Hoorie <nazari.hoorie@gmail.com>
2019-11-23 20:00:40 +0100

    add routes and root containers

--- src/App.js
+++ src/App.js
@@ -1,26 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Route, Link, BrowserRouter as Router, Switch } from 'react-router-dom'
+import { Product } from './containers/Product';
+import { Login } from './containers/Login';
+import { NotFound } from './containers/NotFound';
 import './App.css';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/product" component={Product} />
+        <Route path="/login" component={Login} />
+        <Route component={NotFound} />
+      </Switch>
+    </Router>
   );
 }
 
-export default App;
+export { App };
---
+++ src/containers/NotFound.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class NotFound extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>Not Found</div>
+    )
+  }
+}
+
+export { NotFound };
