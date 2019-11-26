39fbfe2122d4a04994b508e3ade083487913e691
Hoorie <nazari.hoorie@gmail.com>
2019-11-26 18:24:23 +0100

    change product to class

--- src/containers/ProductFunc.js
+++ src/containers/ProductFunc.js
@@ -9,30 +9,25 @@ import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
 import Avatar from '@material-ui/core/Avatar';
 import { Navigation } from '../components/Navigation'
-import { withStyles } from '@material-ui/core/styles';
-
 import axios from 'axios'
 
-const styles = makeStyles(theme => ({
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1,
+  },
   paper: {
     height: 140,
     width: 400,
-  },
-  root: {
-    flexGrow: 1,
   },
   control: {
     padding: theme.spacing(2),
   },
 }));
-
-class ProductClass extends React.Component {
-  constructor(props){
-    super(props)
-    this.state = {ProductInfo : []}
-  }
-  render (){
-    const { classes } = this.props
+const Product = () => 
+{
+    if (localStorage.getItem('token') == null)
+      window.open('/login', '_self')
+    const classes = useStyles();
     return (
       <Grid container className={classes.root} spacing={2} >
         <Grid item xs={12}>
@@ -54,7 +49,7 @@ class ProductClass extends React.Component {
                   <TableCell component="th" scope="row">
                     AirMax
                   </TableCell>
-                  <TableCell align="left">Nike</TableCell>
+                  <TableCell align="left">puma</TableCell>
                   <TableCell align="left">
                     <Avatar alt="Remy Sharp" src="https://material-ui.com/static/images/avatar/1.jpg" />
                   </TableCell>
@@ -68,7 +63,6 @@ class ProductClass extends React.Component {
         </Grid>
       </Grid>
     );
-  }
 }
-const Product = withStyles(styles)(ProductClass)
-export { Product } ;
+
+export { Product }
